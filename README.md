809614b5b4aa0b8f3942debdc64d9609c828d83c
devmobil <devmobil@Macs-MacBook-Pro.local>
2022-07-29 21:13:58 +0300

    parking lot done

---
+++ README.md
@@ -0,0 +1,32 @@
+##Parking Lot
+
+Task: implement a parking lot system with the grid 
+
+
+##### PARKING LOT STRUCTURE
+<b> H </b> - Handicap lot <br>
+<b>C </b>- Car lot <br>
+<b>M</b> - Handicap lot<br>
+<b>T</b> - Truck lot
+
+*| *| *
+------------- | -------------|----
+H1  | H2 | H3
+CA  | C2 | C3
+M1 | M2 | M3
+T1  |  | 
+
+
+- A vehicle can only be parked at the designated zone e.g car on car lot (c1-c3) <br/>
+- Once a vehicle is parked the spot cannot be used until the car is moved<br/>
+- One all the spaces are filled the system should return "no more spaces available"
+
+###How to run
+
+1. Clone repository
+2. `iex parking_lot.exs`
+3. `{:ok, pid} = GenServerService.start_link`
+#### vehicles can be (car,handicap,motor_cycle,truck)
+4. `ParkingLot.find_parking(pid, "car")`
+#### To remove car from lot
+5. `ParkingLot.remove_from_parking(pid, "car", 2)`
---
+++ parking_lot.exs
@@ -0,0 +1,89 @@
+defmodule GenServerService do
+  use GenServer
+
+  @parking_lot %{
+    "handicap" => %{0 => "", 1 => "", 2 => ""},
+    "motor_cycle" => %{0 => "", 1 => "", 2 => "", 3 => "", 4 => ""},
+    "car" => %{0 => "x", 1 => "", 2 => "", 3 => ""},
+    "truck" => %{0 => ""}
+  }
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, @parking_lot)
+  end
+
+  def __init__(map) do
+    {:ok, map}
+  end
+
+  def park(pid, row, column, value) do
+    item_accessor = [row, column, value]
+    GenServer.cast(pid, item_accessor)
+  end
+
+  def view_parking(pid) do
+    GenServer.call(pid, :view_parking)
+  end
+
+  def move_from_parking(pid, row, column) do
+    item = [row, column]
+    GenServer.cast(pid, {:move, item})
+  end
+
+  def stop(pid) do
+    GenServer.stop(pid, :normal, :infinity)
+  end
+
+  def terminate(_reason, map) do
+    :ok
+  end
+
+  def handle_cast({:move, item}, map) do
+    row = item |> Enum.at(0)
+    column = item |> Enum.at(1)
+    value = ""
+    new_map = put_in(map[row][column], value)
+    {:noreply, new_map}
+  end
+
+  def handle_cast(item, map) do
+    row = item |> Enum.at(0)
+    column = item |> Enum.at(1)
+    value = item |> Enum.at(2)
+    new_map = put_in(map[row][column], value)
+    {:noreply, new_map}
+  end
+
+  def handle_call(:view_parking, from, map) do
+    {:reply, map, map}
+  end
+end
+
+defmodule ParkingLot do
+  @moduledoc """
+  x marks a parked vehicle
+  """
+  def find_parking(pid, type) do
+    parking = pid |> GenServerService.view_parking()
+
+    spaces =
+      parking[type]
+      |> Enum.filter(fn {_, v} -> v == "" end)
+      |> Enum.map(fn {k, _} -> k end)
+
+    case length(spaces) >= 1 do
+      true ->
+        available = spaces |> List.first()
+        pid |> GenServerService.park(type, available, "x")
+        pid |> GenServerService.view_parking()
+
+      false ->
+        "No more spaces available for " <> type
+    end
+  end
+
+  def remove_from_parking(pid, type, parking_spot) do
+    pid |> GenServerService.move_from_parking(type, parking_spot)
+    pid |> GenServerService.view_parking()
+  end
+end
